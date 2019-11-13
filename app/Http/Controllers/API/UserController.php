4a8cf7591c60e229fde7e23989fd63ee065d68a7
azdine <farisazzeddine3@gmail.com>
2019-11-13 17:09:23 +0100

    13/11

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,18 +1,13 @@
 <?php
 
-// namespace App\Http\Controllers\API;
-// use Illuminate\Validation\Validator;
-// use Illuminate\Http\Request;
-// use App\Http\Controllers\Controller;
-// use Illuminate\Support\Facades\Auth;
-
 namespace App\Http\Controllers\API;
-use Illuminate\Http\Request; 
-use App\Http\Controllers\Controller; 
-use App\User; 
-use Illuminate\Support\Facades\Auth; 
-use \Validator;
-use App\Http\Requests;
+use Validator;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+// use Illuminate\Support\Facades\Validator;
+
 
 
 
@@ -42,17 +37,17 @@ class UserController extends Controller
 
     public function register(Request $request){
         $validator = Validator::make($request->all(),[
-            'name' => 'requierd',
-            'prenom' => 'requierd',
-            'email' => 'requierd|email',
-            'cin' => 'requierd',
-            'password' => 'requierd',
-            'c_password' => 'requierd|same:password',           
+            'name' => 'required',
+            'prenom' => 'required',
+            'email' => 'required|email',
+            'cin' => 'required',
+            'password' => 'required',
+            'c_password' => 'required|same:password',           
         ]);
         if($validator->fails()){
             return response()->json(['error'=>$validator->errors()], 401);
         }
-        $input = $request->all();
+        $input = $request->except('c_password');
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] = $user->createToken('MyApp')->accessToken;
